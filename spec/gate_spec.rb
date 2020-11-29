aa06370215ecfe4b97ccca8a40d512b9d1f4b3ae
TTRHSGW <thisisnotajob2015@gmail.com>
2020-11-29 22:59:51 +0900

    Fix gate spec let

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -1,18 +1,19 @@
 require_relative "../lib/gate"
 require_relative "../lib/ticket"
 
-describe Gate do
+RSpec.describe Gate do
+  let(:umeda_gate) { Gate.new(:umeda) }
+  let(:mikuni_gate) { Gate.new(:mikuni) }
+  let(:ticket) { Ticket.new(150) }
+
   describe 'Gate new' do
     it 'can new Gate instance' do
-      umeda_gate = Gate.new(:umeda)
       expect(umeda_gate).to be_an_instance_of(Gate)
     end
   end
 
   describe '#enter' do
     it "enter the ticket to gate and stamp_at is enter station's name" do
-      umeda_gate = Gate.new(:umeda)
-      ticket = Ticket.new(150)
       umeda_gate.enter(ticket)
       expect(ticket.stamped_at).to eq :umeda
     end
@@ -20,9 +21,6 @@ describe Gate do
 
   describe '#exit' do
     it "return false exit when fare not enough" do
-      umeda_gate = Gate.new(:umeda)
-      mikuni_gate = Gate.new(:mikuni)
-      ticket = Ticket.new(150)
       umeda_gate.enter(ticket)
       expect(mikuni_gate.exit(ticket)).to be false
     end
