3655d6b0d3cbe012aaa13e493a69499191f55eae
TTRHSGW <thisisnotajob2015@gmail.com>
2020-12-02 20:48:23 +0900

    Add deep freeze spec

--- spec/deep_freezable_spec.rb
+++ spec/deep_freezable_spec.rb
@@ -4,8 +4,10 @@ require_relative '../lib/team'
 
 RSpec.describe DeepFreezable do
   describe '#deep_freeze' do
-    it '' do
-
+    it 'array equal' do
+      expect(Team::COUNTRIES).to eq ['Japan', 'US', 'India']
+      expect(Team::COUNTRIES.frozen?).to be true
+      expect(Team::COUNTRIES.all? { |country| country.frozen? }).to be true
     end
   end
 end
