36e18f7b09358e581ec0ca1ace7d43d78a41f99a
TTRHSGW <thisisnotajob2015@gmail.com>
2020-12-05 09:48:11 +0900

    Add prepend module A to Product class

--- lib/product.rb
+++ lib/product.rb
@@ -1,8 +1,18 @@
 require_relative "../lib/loggable_module.rb"
 
+module A
+  def to_s
+    "<A> #{super}"
+  end
+end
+
 class Product
   include Loggable
+  prepend A
 
+  def to_s
+    "<Product>  #{super}"
+  end
   def title
     log('title is called')
     'A great movie'
@@ -11,3 +21,4 @@ end
 
 product = Product.new
 product.title
+
