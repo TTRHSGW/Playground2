09e6c1fd8e06853f145aa74dda6c42b4139b3859
TTRHSGW <thisisnotajob2015@gmail.com>
2020-12-05 13:30:39 +0900

    add prepend

--- lib/product.rb
+++ lib/product.rb
@@ -6,16 +6,28 @@ module A
   end
 end
 
+module NameDecorator
+  def name
+    "<<#{super}>>"
+  end
+end
+
 class Product
   include Loggable
   prepend A
+  prepend NameDecorator
 
   def to_s
     "<Product>  #{super}"
   end
+
   def title
     log('title is called')
     'A great movie'
+  end
+
+  def name
+    "A great film"
   end
 end
 
