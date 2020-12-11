78544dad808c39966ffd5b811a3d667549e946a5
TTRHSGW <thisisnotajob2015@gmail.com>
2020-12-11 12:04:48 +0900

    Add defaults method for initialize argument

--- lib/gear.rb
+++ lib/gear.rb
@@ -2,22 +2,28 @@ class Gear
   attr_reader :chainring, :cog, :wheel
 
   def initialize(args)
-    @chainring = args.fetch(:chainring, 40)
-    @cog       = args.fetch(:cog, 30)
+    args       = defaults.merge(args)
+    @chainring = args[:chainring]
+    @cog       = args[:cog]
     @wheel     = args[:wheel]
+  end
+
+  def defaults
+    {:chainring => 40, :cog => 30}
   end
 
   def ratio
     chainring / cog.to_f
   end
 
+  def diameter
+    wheel.diameter
+  end
+
   def gear_inches
     ratio * diameter
   end
 
-  def diameter
-    wheel.diameter
-  end
 end
 
 class Wheel
