aa9463659a93b85d65da3bd7277d3fca700203cf
TTRHSGW <thisisnotajob2015@gmail.com>
2020-12-15 10:46:06 +0900

    Add TripCoordinator class and Driver class

--- lib/trip.rb
+++ lib/trip.rb
@@ -1,7 +1,22 @@
 class Trip
   attr_reader :bicycles, :customers, :vehicle
 
-  def prepare(mechanic)
-    mechanic.prepare_bicycles(bicycles)
+  def prepare(preparers)
+    preparers.each {|preparer| preparer.prepare_trip(self)}
+  end
+end
+
+class TripCoordinator
+  def prepare_trip(trip)
+    buy_food(trip.customer)
+  end
+
+  def buy_food
+  end
+end
+
+class Driver
+  def prepare_trip(trip)
+
   end
 end
