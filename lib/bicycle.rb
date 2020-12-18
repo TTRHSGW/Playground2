0d88b3983e4a126d7705e18a83a98ce3cd8d642e
TTRHSGW <thisisnotajob2015@gmail.com>
2020-12-18 12:10:44 +0900

    Add style attr to Bicycle

--- lib/bicycle.rb
+++ lib/bicycle.rb
@@ -1,16 +1,24 @@
 class Bicycles
-  attr_reader :size, :tape_color
+  attr_reader :style, :size, :tape_color,
+              :front_shock, :rear_shock
 
   def initialize(args)
-    @size = args[:size]
-    @tape_color = args[:size]
+    @style       = args[:style]
+    @size        = args[:size]
+    @tape_color  = args[:size]
+    @front_shock = args[:front_shock]
+    @rear_shock  = args[:rear_shock]
   end
 
   def spares
-    {
-      chain: '10-speed',
-      tire_size: '23',
-      tape_color: tape_color
-    }
+    if style == :road
+      { chain: '10-speed',
+        tire_size: '23',
+        tape_color: tape_color }
+    else
+      { chain: '10-speed',
+        tire_size: '2.1',
+        rear_shock: rear_shock }
+    end
   end
 end
