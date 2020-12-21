b52cc06ca1506d05682943b0d687ebc68df3083d
Tatsuru <TTRHSGW@users.noreply.github.com>
2020-12-21 23:14:35 +0900

    Add schedulable? scheduled? lead_days method to Bicycle

--- lib/bicycle.rb
+++ lib/bicycle.rb
@@ -2,10 +2,23 @@ class Bicycle
   attr_reader :size, :chain, :tire_size
 
   def initialize(args={})
+    @schedule  = args[:schedule] || Schedule.new
     @size      = args[:size]
     @chain     = args[:chain] || default_chain
     @tire_size = args[:tire_size] || default_tire_size
     post_initialize(args)
+  end
+  
+  def schedulable?(start_date, end_date)
+    !scheduled?(start_date - lead_days, end_date)
+  end
+  
+  def scheduled?(start_date, end_date)
+    schedule.scheduled?(self, start_date, end_date)
+  end
+  
+  def lead_days
+    1
   end
   
   def post_initialize(args)
