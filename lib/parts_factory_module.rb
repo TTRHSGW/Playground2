0e04c7cfd6bd8e9733e3053f669de188f988d98b
Tatsuru <TTRHSGW@users.noreply.github.com>
2020-12-26 18:25:50 +0900

    Add configures for bicycle

--- lib/parts_factory_module.rb
+++ lib/parts_factory_module.rb
@@ -13,3 +13,19 @@ module PartsFactory
       needs_spare: part_config.fetch(2, true))
   end
 end
+
+road_config =
+  [['chain',      '10-speed'],
+   ['tire_size',  '23'],
+   ['tape_color', 'red']]
+
+mountain_config =
+  [['chain',      '10-speed'],
+   ['tire_size',  '2.1'],
+   ['front_shock','Manitou', false],
+   ['rear_shock', 'Fox']]
+
+recumbent_config =
+  [['chain', '9-speed'],
+   ['tire_size', '28'],
+   ['flag', 'tall and orange']]
