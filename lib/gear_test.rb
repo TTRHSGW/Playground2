2a2367966cb18d9f6a40ded9b50abdf3d2359ed7
Tatsuru <TTRHSGW@users.noreply.github.com>
2020-12-29 17:42:23 +0900

    Add minitest for Gear set cog set chainring method with Mock

---
+++ lib/gear_test.rb
@@ -0,0 +1,21 @@
+class GearTest < MiniTest::Unit::TestCase
+  def setup
+    @observer = MiniTest::Mock.new
+    @gear     = Gear.new(
+      chainring: 52,
+      cog: 11,
+      observer: @observer)
+  end
+  
+  def test_notifies_observer_when_cogs_change
+    @observer.expect(:changed, true, [52, 27])
+    @gear.set_cog(27)
+    @observer.verify
+  end
+  
+  def test_notifies_observer_when_chainring_change
+    @observer.expect(:chenged, true,[42, 11])
+    @gear.set_chainring(42)
+    @observer.verify
+  end
+end
