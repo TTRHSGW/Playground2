9ee321224cf18b64722f1241e4eb6fba6b516201
Tatsuru <TTRHSGW@users.noreply.github.com>
2020-12-30 23:05:43 +0900

    Add test calculates gear inches

--- lib/gear_test.rb
+++ lib/gear_test.rb
@@ -1,4 +1,21 @@
+class DiameterDouble
+  def diameter
+    10
+  end
+end
+
 class GearTest < MiniTest::Unit::TestCase
+  def test_calculates_gear_inches
+    gear = Gear.new(
+      chainring: 52,
+      cog: 11,
+      wheel: DiameterDouble.new)
+      
+    assert_in_delta(47.27,
+      gear.gear_inches,
+      0.01)
+  end
+
   def setup
     @observer = MiniTest::Mock.new
     @gear     = Gear.new(
