11680b5210b3834a28a4bda3699bce33c6ea45af
Tatsuru <TTRHSGW@users.noreply.github.com>
2020-12-30 21:25:40 +0900

    Fix prepare method of Trip class

--- lib/trip.rb
+++ lib/trip.rb
@@ -3,17 +3,7 @@ class Trip
 
   def prepare(preparers)
     preparers.each {|preparer|
-      case preparer
-      when Mechanic
-        preparer.prepare_bicycles(bicycles)
-      when TripCoordinator
-        prepare.buy_food(customers)
-      when Driver
-        prepare.gas_up(vehicle)
-        prepare.fill_water_tank(vehicle)
-      end
-    }
-    #preparers.each {|preparer| preparer.prepare_trip(self)}
+      preparer.prepare_trip(self)}
   end
 end
 
